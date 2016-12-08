e4be18cb7408cdbccf312ad1e2dadaa99a25a56b
duks <duks@robooot.com>
2016-12-08 15:55:43 +0800

    Initial Commit

---
+++ MoveAndDelete/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MoveAndDelete
+//
+//  Created by aDu on 2016/12/8.
+//  Copyright © 2016年 aDu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MoveAndDelete/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MoveAndDelete
+//
+//  Created by aDu on 2016/12/8.
+//  Copyright © 2016年 aDu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
